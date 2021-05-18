5583f610de044244b3f5c24467c83b562033b5d9
Enngage <ennspassion@gmail.com>
2021-05-18 10:41:21 +0200

    feat: adds avatar to project name, adds video count under project name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { AppFlexModule } from './flex';
 import { LineClampDirective } from './core/line-clamp.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AvatarModule } from 'ngx-avatar';
 
 @NgModule({
     declarations: [AppComponent, LineClampDirective],
-    imports: [BrowserModule, HttpClientModule, AppFlexModule, FormsModule, ReactiveFormsModule],
+    imports: [BrowserModule, HttpClientModule, AppFlexModule, FormsModule, ReactiveFormsModule, AvatarModule],
     providers: [],
     bootstrap: [AppComponent]
 })
