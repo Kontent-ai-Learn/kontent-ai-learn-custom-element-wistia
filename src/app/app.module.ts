4d4a053748cda41b7f4882ccdaaca76ccf449766
Enngage <ennspassion@gmail.com>
2021-05-20 13:17:17 +0200

    feat: adds angular material + prepares support for wistia video sorting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { AppFlexModule } from './flex';
 import { LineClampDirective } from './core/line-clamp.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AvatarModule } from 'ngx-avatar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent, LineClampDirective],
-    imports: [BrowserModule, HttpClientModule, AppFlexModule, FormsModule, ReactiveFormsModule, AvatarModule],
+    imports: [BrowserModule, HttpClientModule, AppFlexModule, FormsModule, ReactiveFormsModule, AvatarModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent]
 })
