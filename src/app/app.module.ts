b5dad0f43ed5e0da04abf78e4d27225256603674
Enngage <ennspassion@gmail.com>
2022-06-02 13:44:29 +0200

    feat: update to Angular 13, minor design updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { AppFlexModule } from './flex';
 import { LineClampDirective } from './core/line-clamp.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AvatarModule } from 'ngx-avatar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
     declarations: [AppComponent, LineClampDirective],
@@ -19,10 +19,10 @@ import { MatIconModule } from '@angular/material/icon';
         AppFlexModule,
         FormsModule,
         ReactiveFormsModule,
-        AvatarModule,
         BrowserAnimationsModule,
         MatMenuModule,
-        MatIconModule
+        MatIconModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
