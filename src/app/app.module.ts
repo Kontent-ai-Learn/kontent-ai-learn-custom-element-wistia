ae30a6a8714362502ced8e725a85eee13f490146
Richard Sustek <ennspassion@gmail.com>
2022-10-25 10:17:31 +0200

    feat: adds ability to view & copy video id, improves design a bit, updates to Angular 14

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CopyTextComponent } from './components/copy-text.component';
 
 @NgModule({
-    declarations: [AppComponent, LineClampDirective],
+    declarations: [AppComponent, LineClampDirective, CopyTextComponent],
     imports: [
         BrowserModule,
         HttpClientModule,
